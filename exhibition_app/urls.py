63e5ee09d5c9617c4ddc23e6102e206f20cf801b
Poornima Menon <poornimaclt25@gmail.com>
2021-10-23 21:57:53 -0400

    Complete Login/logout and Create function for Exhibition

--- exhibition_app/urls.py
+++ exhibition_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'), 
     path('info/', views.info, name='info'),
     path('artists/', views.artist, name='artist'),
-    path('exhibition/create/' , views.create_exhibition)
+    path('exhibition/' , views.exhibition),
+    path('exhibition/new/', views.new),
+    path('exhibition/submit/' , views.create_exhibition),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- exhibition_app/views.py
+++ exhibition_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from exhibition_app.models import Exhibition
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -12,7 +16,38 @@ def artist(request):
 
 # Exhibition view functions
 
-def create_exhibition(request):
-
-  return render(request , 'exhibition/create.html')
+def exhibition(request):
+  exhibition = Exhibition.objects.filter(user=request.user)
+  return render(request , 'exhibition/prof  ile.html' , {'exhibition' : exhibition})
   
+def new(request):
+  return render (request , 'exhibition/create.html')
+
+def create_exhibition(request):
+  Exhibition.objects.create(
+    title = request.POST['title'],
+    artist_name = request.POST['artist_name'],
+    description = request.POST['description'],
+    date = request.POST['date'],
+    user_id = request.user.id
+  )
+  return redirect('/exhibition')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('/exhibition')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
